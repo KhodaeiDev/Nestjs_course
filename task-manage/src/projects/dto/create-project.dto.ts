0a5ba72ffdd5d438169d5bcee720f0907c50dfe4
khodaeidev <khodaei.dev@gmail.com>
2025-03-01 10:27:59 +0330

    Add validator to dtos

--- task-manage/src/projects/dto/create-project.dto.ts
+++ task-manage/src/projects/dto/create-project.dto.ts
@@ -1,9 +1,12 @@
-import { IsString, Min } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, Min, MinLength } from 'class-validator';
 import projectStatusEnum from '../enums/projectStatusEnum';
 
 export class CreateProjectDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
 
+  @IsEnum(projectStatusEnum)
   status: projectStatusEnum;
 }
