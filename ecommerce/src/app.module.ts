caaaf89fb03c68f8bcdd4a68000662615b1719ed
khodaeidev <khodaei.dev@gmail.com>
2025-05-28 12:08:53 +0330

    Connection to Db

--- ecommerce/src/app.module.ts
+++ ecommerce/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
