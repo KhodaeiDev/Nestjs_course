44a3604e15a201365df72063e7ed0fedcb3165d1
khodaeidev <khodaei.dev@gmail.com>
2025-05-28 11:24:17 +0330

    initial ecommerce Project

--- Social-Media/src/middlewares/headers.js
+++
@@ -1,6 +0,0 @@
-exports.setHeaders = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorizations");
-  next();
-};
--- Social-Media/src/modules/post/post.validator.js
+++
@@ -1,7 +0,0 @@
-const yup = require("yup");
-
-exports.postCreateValidator = yup.object({
-  description: yup
-    .string()
-    .max(2200, "Description can not more than 2200 chars"),
-});
