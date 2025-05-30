d58e5eadbabeca7f27db276a31773506ab72b887
khodaeidev <khodaei.dev@gmail.com>
2025-05-30 11:52:22 +0330

    Dev auth login adn register Dto

---
+++ ecommerce/src/auth/dto/login.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsString()
+  @Matches(/^.{11}$/, { message: 'فرمت شماره موبایل معتبر نیست' })
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
+  mobile: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(16)
+  password: string;
+}
---
+++ ecommerce/src/auth/dto/register.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+
+export class CreateUserDto {
+  @IsString()
+  @Matches(/^.{11}$/, { message: 'فرمت شماره موبایل معتبر نیست' })
+  @Transform(({ value }) => value.trim())
+  @IsNotEmpty()
+  mobile: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 32)
+  name: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(16)
+  password: string;
+}
