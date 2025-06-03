c4044088e98165ab12be51d6cea80e41245f3577
khodaeidev <khodaei.dev@gmail.com>
2025-06-03 09:26:26 +0330

    Dec Crud Address

--- ecommerce/src/address/address.controller.ts
+++ ecommerce/src/address/address.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
@@ -8,8 +16,8 @@ export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
   @Post()
-  create(@Body() createAddressDto: CreateAddressDto) {
-    return this.addressService.create(createAddressDto);
+  create(@Body() createAddressDto: CreateAddressDto, userId: number) {
+    return this.addressService.create(createAddressDto, userId);
   }
 
   @Get()
--- ecommerce/src/address/address.service.ts
+++ ecommerce/src/address/address.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Address } from './entities/address.entity';
+import { EntityNotFoundError, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AddressService {
-  create(createAddressDto: CreateAddressDto) {
-    return 'This action adds a new address';
+  constructor(
+    @InjectRepository(Address)
+    private readonly addressRepository: Repository<Address>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(
+    createAddressDto: CreateAddressDto,
+    userId: number,
+  ): Promise<Address> {
+    try {
+      const user = await this.userRepository.findOneByOrFail({ id: userId });
+
+      const address = this.addressRepository.create({
+        ...createAddressDto,
+        user,
+      });
+
+      return await this.addressRepository.save(address);
+    } catch (err) {
+      if (err instanceof EntityNotFoundError)
+        throw new NotFoundException('کاربر پیدا نشد');
+
+      throw err;
+    }
   }
 
-  findAll() {
-    return `This action returns all address`;
+  async findAll(): Promise<Address[]> {
+    const addresses = await this.addressRepository.find({
+      relations: ['user'],
+    });
+
+    return addresses;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} address`;
+  async findOne(id: number): Promise<Address> {
+    const address = await this.addressRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
+    if (!address) {
+      throw new NotFoundException(`ادرس با آیدی ${id} پیدا نشد`);
+    }
+
+    return address;
   }
 
-  update(id: number, updateAddressDto: UpdateAddressDto) {
-    return `This action updates a #${id} address`;
+  async update(
+    id: number,
+    updateAddressDto: UpdateAddressDto,
+  ): Promise<Address> {
+    const address = await this.findOne(id);
+    Object.assign(address, updateAddressDto);
+
+    return await this.addressRepository.save(address);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} address`;
+  async remove(id: number): Promise<void> {
+    const address = await this.findOne(id);
+    await this.addressRepository.remove(address);
   }
 }
