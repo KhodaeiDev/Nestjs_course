21a35b2907d69994618e921c1110231939171026
khodaeidev <khodaei.dev@gmail.com>
2025-07-31 12:14:03 +0330

    Dev ip limit Tracker Service for ipTracker Middleware

--- ecommerce/src/ip-tracker/ip-tracker.service.ts
+++ ecommerce/src/ip-tracker/ip-tracker.service.ts
@@ -1,13 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { IpTracker } from './entities/ip-tracker.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class IpTrackerService {
-  private readonly requestCount = 10;
-  private readonly window_minute = 1;
-  private readonly block_minute = 1;
+  private readonly MAX_REQUEST = 4;
+  private readonly WINDOW_MINUTE = 1;
+  private readonly BLOCK_MINUTE = 1;
+  private readonly TEHRAN_TIME = 3.5;
+
+  constructor(
+    @InjectRepository(IpTracker)
+    private readonly ipTrackRepository: Repository<IpTracker>,
+  ) {}
 
   async track(ip: string) {
+    // Get time and tehran TimeZone
     const now = new Date();
-    console.log(`ip: ${ip} send request in time : ${now}`);
+    const nowTh = new Date(now.getTime() + this.TEHRAN_TIME * 60 * 60 * 1000);
+
+    // Find Record
+    const record = await this.ipTrackRepository.findOne({ where: { ip } });
+
+    // Block Check
+    if (record?.isBlocked && record?.blockUtil >= nowTh) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.TOO_MANY_REQUESTS,
+          error: 'Too Many Request',
+          message: `شما برای مدت ${this.BLOCK_MINUTE} دقیقه محدود شده اید`,
+        },
+        HttpStatus.TOO_MANY_REQUESTS,
+      );
+    }
+    // Create Record
+    if (!record) {
+      const newRecord = this.ipTrackRepository.create({
+        ip,
+        windowStart: now,
+        requestCount: 1,
+        isBlocked: false,
+        blockUtil: null,
+      });
+
+      await this.ipTrackRepository.save(newRecord);
+      return;
+    }
+
+    // End Record Time After 1 min
+    const windowEnd = new Date(
+      record.windowStart.getTime() +
+        this.WINDOW_MINUTE * 60 * 1000 +
+        this.TEHRAN_TIME * 60 * 60 * 1000,
+    );
+
+    // Check Record for reset Or add to Request Count and Block Condition
+    if (nowTh > windowEnd) {
+      record.windowStart = now;
+      record.requestCount = 1;
+      record.blockUtil = null;
+      record.isBlocked = false;
+    } else {
+      record.requestCount += 1;
+      if (record.requestCount >= this.MAX_REQUEST) {
+        record.isBlocked = true;
+        record.blockUtil = new Date(
+          nowTh.getTime() + this.BLOCK_MINUTE * 60 * 1000,
+        );
+      }
+    }
+    await this.ipTrackRepository.save(record);
   }
 }
